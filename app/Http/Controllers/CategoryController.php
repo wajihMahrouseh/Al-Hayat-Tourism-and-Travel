8050645c76fa111f2d38cf23f9dd57ba77987fe5
wajihMahrouseh <wajiho.mahrouseh@gmail.com>
2023-12-20 23:48:36 +0300

    programming category component

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Spatie\QueryBuilder\QueryBuilder;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
+use App\Http\Resources\CategoriesListResource;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\CategoryDetailsResource;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class CategoryController extends Controller
 {
@@ -13,7 +18,20 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categoriesQuery = QueryBuilder::for(Category::class)
+            ->select(['id', 'name', 'created_at'])
+            ->allowedFilters(['name'])
+            ->latest();
+
+        $categories = request()->page === null ? $categoriesQuery->get() : $categoriesQuery->paginate(request()->input('limit'));
+
+        // Get the pagination meta information using the function
+        $paginationMeta = $this->generatePaginationMeta($categories);
+
+        return response()->json([
+            'meta' => $paginationMeta,
+            'data' => CategoriesListResource::collection($categories),
+        ]);
     }
 
     /**
@@ -21,7 +39,17 @@ class CategoryController extends Controller
      */
     public function store(StoreCategoryRequest $request)
     {
-        //
+        $category = Category::create($request->validated());
+
+        // Handle photo upload
+        if ($request->hasFile('photo')) {
+            $photo = $request->file('photo');
+            $category->addMedia($photo)->toMediaCollection('photos');
+        }
+
+        return response()->json([
+            'message' => trans('messages.add'),
+        ], Response::HTTP_CREATED);
     }
 
     /**
@@ -29,7 +57,9 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        return response()->json([
+            'data' => new CategoryDetailsResource($category),
+        ]);
     }
 
     /**
@@ -37,7 +67,11 @@ class CategoryController extends Controller
      */
     public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category->update($request->validated());
+
+        return response()->json([
+            'message' => trans('messages.edit'),
+        ]);
     }
 
     /**
@@ -45,6 +79,35 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return response()->json([
+            'message' => trans('messages.delete'),
+        ]);
+    }
+
+
+    // Function to generate meta information for pagination
+    public function generatePaginationMeta(LengthAwarePaginator $paginator = null)
+    {
+        if ($paginator) {
+            return [
+                "current_page" => $paginator->currentPage(),
+                "last_page" => $paginator->lastPage(),
+                "per_page" => $paginator->perPage(),
+                "path" => $paginator->path(),
+                "fragment" => $paginator->fragment(),
+                "first_page_url" => $paginator->url(1),
+                "last_page_url" => $paginator->url($paginator->lastPage()),
+                "next_page_url" => $paginator->nextPageUrl(),
+                "prev_page_url" => $paginator->previousPageUrl(),
+                "from" => $paginator->firstItem(),
+                "to" => $paginator->lastItem(),
+                "total" => $paginator->total(),
+                'links' => $paginator->render(),
+            ];
+        }
+
+        return null;
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,12 +3,18 @@
 namespace App\Models;
 
 use App\Models\Site;
+use Spatie\MediaLibrary\HasMedia;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Category extends Model
+class Category extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
+
+
+    protected $fillable = ['name'];
 
 
     public function sites()
