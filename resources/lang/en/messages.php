76dc9a6e663e36c79a6d57301d2e8945bb2642c0
wajihMahrouseh <wajiho.mahrouseh@gmail.com>
2023-12-20 23:45:34 +0300

    prepare lang messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | 
+    | 
+    | 
+    |
+    */
+
+    'add' => 'added successfully',
+    'edit' => 'edited successfully',
+    'delete' => 'deleted successfully',
+];
